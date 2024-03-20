aba5da9a65383223f3bb428ba7962d3d3ef530ee
science <science@pop-os.localdomain>
2024-03-19 19:51:35 -0600

    Resolve merge conflict

--- processing.py
+++ processing.py
@@ -1,5 +1,4 @@
 import time
-
 import boto3
 import cv2
 import numpy as np
@@ -9,77 +8,49 @@ from ultralytics import YOLO
 # Initialize AWS credentials and Kinesis Video client
 region_name = 'us-east-1'
 stream_name = 'camera_100'
+model_path= "model/best.pt"
 
 session = boto3.Session()
 kinesis_video_client = session.client('kinesisvideo', region_name=region_name)
 
+kvs= boto3.client("kinesisvideo")
+endpoint= kvs.get_data_endpoint(
+    APINAME="GET_HLS_STREAMING_SESSION_URL",
+    stream_name=stream_name
+)["DataEndpoint"]
 
-def get_kinesis_video_stream_frames(client, stream_name):
+print(endpoint)
+kvam = boto3.client("kinesis-video-archived-media", endpoint_url=endpoint)
 
-    response = client.get_data_endpoint(
-        StreamName=stream_name,
-        APIName='GET_MEDIA'
-    )
+url = kvam.get_hls_stream_session_url(stream_name=stream_name,PlaybackMode="LIVE")["HLSStreamingSessionURL"]
 
-    video_data_endpoint = response['DataEndpoint']
+vcap = cv2.VideoCapture(url)
 
-    # Create a Kinesis video media client
-    kinesis_video_media_client = session.client('kinesis-video-media',
-                                                region_name=region_name,
-                                                endpoint_url=video_data_endpoint)
+while (True):
+    ret, frame = vcap.read()
+    if frame is not None:
+        model = YOLO(model_path)
+        results = model(frame)[0]
+        # Draw bounding boxes and labels
 
-    # Retrieve video stream
-    response = kinesis_video_media_client.get_media(
-        StreamName=stream_name,
-        StartSelector={'StartSelectorType': 'NOW'}
-    )
+        img_objects = []
+        for box, conf, class_id in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
+            x1, y1, x2, y2 = box
+            if conf > 0.90:
+                img_objects.append(model_path.names[int(class_id)].upper())
+        if 'dog' in img_objects or 'person' in img_objects:
+            client = boto3.client("sns")
+            client.publish(
+                TopicArn="arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0",
+                Message="A dog was detected alone",
+            )
+        if cv2.waitKey(22)& 0xFF == ord("q"):
+            break
+        else:
+            print("frame is None")
+            break
 
-    return response['Payload'].read()
+vcap.release()
+cv2.destroyAllWindows()
+print("Video stream is closed")
 
-
-def load_model(model_path):
-    model = YOLO(model_path)
-    return model
-
-
-def object_detector(frame_data, model_path):
-    # Encoding the frame into numpy array
-    nparr = np.frombuffer(frame_data, np.uint8)
-
-    # Decode the frame
-    frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    # Processing the Frame with YOLO model
-    # You can replace this with actual YOLO inference code
-    model = YOLO(model_path)
-    results = model(frame)[0]
-    # Draw bounding boxes and labels
-
-    img_objects = []
-    for box, conf, class_id in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
-        x1, y1, x2, y2 = box
-        if conf > 0.90:
-            img_objects.append(model_path.names[int(class_id)].upper())
-    if 'dog' in img_objects or 'person' in img_objects:
-        return True
-    else:
-        return False
-
-
-def send_sns_email():
-    # Create an SNS client
-    client = boto3.client("sns")
-
-    # Send your sms message.
-    client.publish(
-        TopicArn="arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0",
-        Message="A dog was detected alone",
-    )
-
-
-model = load_model('model/best.pt')
-frames = get_kinesis_video_stream_frames(kinesis_video_client, stream_name)
-
-# Ensure frames are not empty
-if not frames:
-    raise Exception("Frames data is empty")
