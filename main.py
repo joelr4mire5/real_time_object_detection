1de4be78d02ffea8a92bdb2c33b51049288bac11
science <science@pop-os.localdomain>
2024-03-19 22:27:36 -0600

    adding new featurev4

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import av
 from PIL import Image
 # to not exceed the S3 API rate limits
 import time
+import PIL
 
 
 # Initialize AWS credentials and Kinesis Video client
@@ -27,36 +28,39 @@ folder_name = 'camera_100'
 # Create a Bucket resource
 bucket = s3.Bucket(bucket_name)
 
-objects = [(obj.last_modified, obj.key) for obj in bucket.objects.filter(Prefix=folder_name)]
+while True:
+    objects = [(obj.last_modified, obj.key) for obj in bucket.objects.filter(Prefix=folder_name)]
+    objects.sort(reverse=True)
+    newest_object_name = objects[0][1]
+    newest_image_path = 'newest_image.jpg'
 
-objects.sort(reverse=True)
+    # Download the newest object
+    bucket.download_file(newest_object_name, newest_image_path)
 
-newest_object_name = objects[0][1]
-newest_image_path = 'newest_image.jpg'
+    # Open the image file
+    img = Image.open(newest_image_path)
 
-# Download the newest object
-bucket.download_file(newest_object_name, newest_image_path)
-img = Image.open(newest_image_path)
+    # Run image through YOLO model
+    model_path = 'model/best.pt'
+    model = YOLO(model_path)
+    results = model.predict(img)[0]
 
-model_path = 'model/best.pt'
-model = YOLO(model_path)
-results = model.predict(img)[0]
+    # Draw bounding boxes and labels
+    img_objects = []
+    for box, conf, class_id in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
+        x1, y1, x2, y2 = box
+        if conf > 0.90:
+            img_objects.append(model.names[int(class_id)].upper())
 
-# Draw bounding boxes and labels
-img_objects = []
-for box, conf, class_id in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
-    x1, y1, x2, y2 = box
-    if conf > 0.90:
-        img_objects.append(model.names[int(class_id)].upper())
+    # If dog is detected, send SNS notification
+    if 'dog' in img_objects or 'person' in img_objects:
+        client = boto3.client("sns",
+                              region_name='your-region-name')  # Replace 'your-region-name' with your actual region
+        topic_arn = "arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0"
+        client.publish(
+            TopicArn=topic_arn,
+            Message="A dog was detected alone"
+        )
 
-# If dog is detected, send SNS notification
-if 'dog' in img_objects or 'person' in img_objects:
-    client = boto3.client("sns",
-                          region_name='your-region-name')  # Replace 'your-region-name' with your actual region
-    topic_arn = "arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0"
-    client.publish(
-        TopicArn=topic_arn,
-        Message="A dog was detected alone"
-    )
-    time.sleep(600)
-
+    # Add delay before the next iteration
+    time.sleep(60)
