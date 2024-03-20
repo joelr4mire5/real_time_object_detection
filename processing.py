287a6517b822ea2a2f765b833ff1e13c4254210f
science <science@pop-os.localdomain>
2024-03-19 21:13:30 -0600

    capturing data using av

--- processing.py
+++ processing.py
@@ -1,8 +1,9 @@
 import time
 import boto3
-import cv2
+import av
 import numpy as np
 from ultralytics import YOLO
+import cv2
 
 
 # Initialize AWS credentials and Kinesis Video client
@@ -25,25 +26,14 @@ print(endpoint)
 kvam = boto3.client("kinesis-video-archived-media", endpoint_url=endpoint, region_name=region_name)
 
 url = kvam.get_hls_streaming_session_url(StreamName=stream_name, PlaybackMode="LIVE")["HLSStreamingSessionURL"]
-vcap = cv2.VideoCapture(url)
 model = YOLO(model_path)
+container = av.open(url)
 
-if not vcap.isOpened():
-    print("Error opening video stream")
-else:
-    print("Video stream opened successfully")
+stream = next(s for s in container.streams if s.type == 'video')
 
-while (True):
-    ret, frame = vcap.read()
-
-    if not ret or frame is None:
-        print("Frame could not be captured")
-        continue
-
-
-
-    results = model(frame)[0]
-    # Draw bounding boxes and labels
+for frame in container.decode(stream):
+    img_array = frame.to_ndarray(format='bgr24')
+    results = model(img_array)[0]
 
     img_objects = []
     for box, conf, class_id in zip(results.boxes.xyxy, results.boxes.conf, results.boxes.cls):
@@ -51,19 +41,17 @@ while (True):
         if conf > 0.90:
             img_objects.append(model_path.names[int(class_id)].upper())
     if 'dog' in img_objects or 'person' in img_objects:
-        client = boto3.client("sns",region_name=region_name)
+        client = boto3.client("sns", region_name=region_name)
         client.publish(
-                TopicArn="arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0",
-                Message="A dog was detected alone",
+            TopicArn="arn:aws:sns:us-east-1:905418005302:dog_detection:10e1c03a-8211-49d6-bc94-051c196651a0",
+            Message="A dog was detected alone",
         )
         print("message sent")
-    if cv2.waitKey(22)& 0xFF == ord("q"):
+    if np.all(np.array(cv2.waitKey(22)) & 0xFF == ord("q")):
         break
     else:
         print("frame is None")
         break
 
-vcap.release()
-cv2.destroyAllWindows()
 print("Video stream is closed")
 
